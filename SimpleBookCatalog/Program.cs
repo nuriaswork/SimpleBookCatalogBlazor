4b20d5487efada933af6ced1a7bf2c5c5283dc5a
WKE\nuria.cambronero <nuria.cambronero@external.wolterskluwer.com>
2026-02-17 13:28:24 +0100

    Scope Validation when registering dependencies

--- SimpleBookCatalog/Program.cs
+++ SimpleBookCatalog/Program.cs
@@ -6,6 +6,30 @@ using SimpleBookCatalog.Infraestructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Enable service provider validation in Development to catch DI lifetime issues early
+//i.e., validate scopes (AddScoped, AddTransient, AddSingleton, etc.)
+//ONLY IN NON-PROD as it adds overhead
+builder.Host.UseDefaultServiceProvider((context, options) =>
+{
+    var isDev = context.HostingEnvironment.IsDevelopment();
+    options.ValidateScopes = isDev;
+    options.ValidateOnBuild = isDev;
+});
+/* With configuration toggle in appsettings.json:
+// Read toggle from configuration 
+var enableValidationFromConfig = builder.Configuration.GetValue<bool?>("ServiceProviderValidation:Enable");
+var validateOnBuildFromConfig = builder.Configuration.GetValue<bool?>("ServiceProviderValidation:ValidateOnBuild");
+// If the config key is not present, enable validation in Development by default
+var enableValidation = enableValidationFromConfig ?? builder.Environment.IsDevelopment();
+var validateOnBuild = validateOnBuildFromConfig ?? enableValidation;
+// Enable service provider validation based on the configuration toggle:
+builder.Host.UseDefaultServiceProvider((context, options) =>
+{
+    options.ValidateScopes = enableValidation;
+    options.ValidateOnBuild = validateOnBuild;
+});
+ */
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
